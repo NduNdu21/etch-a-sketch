fb1588e1118bd8f725a03905fef4df4ad821ef76
NduNdu21 <ndumisobm@icloud.com>
2025-01-29 10:54:33 +0000

    Revised div create

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ const container = document.querySelector(".container")
 
 //creating grid squares
 for(let i = 0; i < 16; i++) {
-    container.appendChild(document.createElement("div"))
+    const newDiv = document.createElement("div")
+    container.appendChild(newDiv)
 }
