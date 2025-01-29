ac06faa469efbcd21bfa9d390282ebe1caa7fe30
NduNdu21 <ndumisobm@icloud.com>
2025-01-29 13:48:59 +0000

    Stylin divs to visualise them

--- script.js
+++ script.js
@@ -4,5 +4,10 @@ const container = document.querySelector(".container")
 //creating grid squares
 for(let i = 0; i < 16; i++) {
     const newDiv = document.createElement("div")
+
+    newDiv.style.borderStyle = "solid"
+    newDiv.style.height = "30px"
+    newDiv.style.width = "30px"
+
     container.appendChild(newDiv)
 }
