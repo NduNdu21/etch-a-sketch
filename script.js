64347cee7853bc27d34e5e0a30af29659e0617af
NduNdu21 <ndumisobm@icloud.com>
2025-01-29 16:20:35 +0000

    Set container to fixed width

--- script.js
+++ script.js
@@ -6,8 +6,5 @@ for(let i = 0; i < 16; i++) {
     const newDiv = document.createElement("div")
 
     newDiv.style.borderStyle = "solid"
-    newDiv.style.height = "30px"
-    newDiv.style.width = "30px"
-
     container.appendChild(newDiv)
 }
