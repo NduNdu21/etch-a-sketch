acdc72188645be84b4550a5f68fe153852a44ac0
NduNdu21 <ndumisobm@icloud.com>
2025-02-05 17:00:56 +0000

    pushing latest revisions so that I can start afresh

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 //select the container div
 const container = document.querySelector(".container")
 
+//calculate how many grid squares
+const containerWidth = container.getAttribute("width")
+
+const divWidth = containerWidth / 16
+
 //creating grid squares
 for(let i = 0; i < 16; i++) {
     const newDiv = document.createElement("div")
 
     newDiv.style.borderStyle = "solid"
+    newDiv.style.width = divWidth + "px"
+    newDiv.style.height = divWidth + "px"
+
     container.appendChild(newDiv)
 }
+
+//hover to change color
+const divs = document.querySelectorAll("div")
+
+divs.forEach((div) => {
+    div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "grey"
+    })
+})
