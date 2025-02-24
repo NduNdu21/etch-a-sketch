6c66ae6c466c0dc2ca686b6cf27a03c477c1b0f7
NduNdu21 <ndumisobm@icloud.com>
2025-02-24 13:35:38 +0000

    Moved all methods and calculations into the button event listener

--- script.js
+++ script.js
@@ -3,32 +3,33 @@ const container = document.querySelector(".container")
 
 //prompt to get grid size
 const button = document.querySelector("button")
-let size 
+let size
 
 button.addEventListener("click", () => {
     size = prompt("Choose your grid size:\nEnter a number of squares.\n\nNote: The max limit is 100")
-})
 
-//calculate how many grid squares
-const containerWidth = 960
+    //calculate how many grid squares
+    const containerWidth = 960
 
-const divWidth = containerWidth / size
+    const divWidth = containerWidth / size
 
-//creating grid squares
-for (let i = 0; i < size * size; i++) {
-    const newDiv = document.createElement("div")
+    //creating grid squares
+    for (let i = 0; i < size * size; i++) {
+        const newDiv = document.createElement("div")
 
-    newDiv.style.width = divWidth + "px"
-    newDiv.style.height = divWidth + "px"
+        newDiv.style.width = divWidth + "px"
+        newDiv.style.height = divWidth + "px"
 
-    container.appendChild(newDiv)
-}
+        container.appendChild(newDiv)
+    }
 
-//hover to change color
-const divs = container.querySelectorAll("div")
+    //hover to change color
+    const divs = container.querySelectorAll("div")
 
-divs.forEach((div) => {
-    div.addEventListener("mouseover", () => {
-        div.style.backgroundColor = "grey"
+    divs.forEach((div) => {
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = "grey"
+        })
     })
 })
+
