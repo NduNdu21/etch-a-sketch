c958d0210ce2b4ab4f9526d80e613b545da7d1ee
NduNdu21 <ndumisobm@icloud.com>
2025-02-24 13:32:17 +0000

    For loop values were hardcoded, updated

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ const container = document.querySelector(".container")
 
 //prompt to get grid size
 const button = document.querySelector("button")
-let size = 16
+let size 
 
 button.addEventListener("click", () => {
     size = prompt("Choose your grid size:\nEnter a number of squares.\n\nNote: The max limit is 100")
-    
 })
 
 //calculate how many grid squares
@@ -16,7 +15,7 @@ const containerWidth = 960
 const divWidth = containerWidth / size
 
 //creating grid squares
-for (let i = 0; i < 16 * 16; i++) {
+for (let i = 0; i < size * size; i++) {
     const newDiv = document.createElement("div")
 
     newDiv.style.width = divWidth + "px"
